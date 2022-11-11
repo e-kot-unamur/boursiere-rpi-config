b9956a20c8605ea3489f709eeb655ea4138e5c58
mdevolde <martin.devolder@student.unamur.be>
2022-11-11 17:35:54 +0100

    Create rpi-config.sh
    rpi-config.sh v1.0

---
+++ rpi-config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# configuration of a raspberry at his first start (needs to be run as root)
+
+# variable to change according to your needs
+user="pi" # name of your user on the raspberry (probably pi)
+
+# ssh activation
+systemctl enable ssh
+systemctl start ssh
+
+# sleep mode desactivation
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# creates shortcuts to boursiere website on desktop 
+cat >/home/$user/Desktop/boursiere_website.desktop <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Name=Boursiere website
+Type=Link
+URL=https://boursiere.e-kot.be/
+Icon=text-html
+EOF
+
+# installs firefox
+apt-get install -y firefox-esr firefox-esr-l10n-fr
+
+# reboot to apply changes
+shutdown -r now
